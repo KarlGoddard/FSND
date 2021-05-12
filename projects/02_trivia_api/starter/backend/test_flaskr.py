7902b53045bcc12b3d8022eb119e11a4f20dc4b3
Karl Goddard <karlgoddard@hotmail.co.uk>
2021-05-12 20:05:24 +0100

    feat: added database details and completed first def test for successful call to /categories

--- projects/02_trivia_api/starter/backend/test_flaskr.py
+++ projects/02_trivia_api/starter/backend/test_flaskr.py
@@ -14,8 +14,10 @@ class TriviaTestCase(unittest.TestCase):
         """Define test variables and initialize app."""
         self.app = create_app()
         self.client = self.app.test_client
-        self.database_name = "trivia_test"
-        self.database_path = "postgres://{}/{}".format('localhost:5432', self.database_name)
+        # self.database_name = "trivia_test"
+        self.database_path = "postgres://{}:{}@{}/{}".format('postgres','pass1','localhost:5432', 'trivia_test')
+        # self.database_name = "trivia_test"
+        # self.database_path = "postgres://{}/{}".format('localhost:5432', self.database_name)
         setup_db(self.app, self.database_path)
 
         # binds the app to the current context
@@ -29,13 +31,19 @@ class TriviaTestCase(unittest.TestCase):
         """Executed after reach test"""
         pass
 
-    def test_categories
-    def test_questions
-    def test_delete
-    def test_add
-    def test_search
-    def test_quiz
-    
+    def test_categories(self):
+        res = self.client().get('/categories')
+        data = json.loads(res.data)
+
+        self.assertEqual(res.status_code, 200)
+        self.assertEqual(data['success'], True)
+
+    # def test_questions
+    # def test_delete
+    # def test_add
+    # def test_search
+    # def test_quiz
+
     """
     TODO
     Write at least one test for each test for successful operation and for expected errors.
